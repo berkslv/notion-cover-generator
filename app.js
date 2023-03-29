6a9d24714ea383600837e68c669aab55e81c20fe
berkslv <berkslv@gmail.com>
2023-03-29 14:57:27 +0300

    Added Express.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', indexRouter);
+
+module.exports = app;
