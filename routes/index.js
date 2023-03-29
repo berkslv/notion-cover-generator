73ef7319de844b61fe84304a074cc79f91306a92
berkslv <berkslv@gmail.com>
2023-03-29 15:21:33 +0300

    Added green theme

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const themes = require("../themes");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send({ hello: "world" });
+router.get("/", function (req, res, next) {
+  const theme = req.query.theme || "green";
+  const text = req.query.text || "Cover title";
+
+  res.send(themes[theme](text));
 });
 
 module.exports = router;
