72b1d1f1ef93ae6e10f2b2e134e8ad94b38930a7
berkslv <berkslv@gmail.com>
2023-03-29 17:05:58 +0300

    Added docs

--- validator/params.js
+++ validator/params.js
@@ -1,9 +1,9 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const params = Joi.object({
-    theme: Joi.string().valid('green', 'blue', 'red', 'yellow', 'purple'),
-    type: Joi.string().valid('wave','circle','blob','stacked'),
-    text: Joi.string(),
-})
+  theme: Joi.string().valid("green", "blue", "red", "yellow", "purple"),
+  type: Joi.string().valid("wave", "circle", "blob", "stacked"),
+  text: Joi.string(),
+});
 
 module.exports = params;
